640de9aa5454bd231d28274233d947da13bfb68a
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2014-02-18 15:15:48 -0500

    lecture 4: show, edit, delete (and getting comments to work on heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+	has_many :post, :dependent => :destroy
+	has_many :comments, :dependent => :destroy
 end
