a15a5faa4234579847acf314bde30c507c5d8986
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2014-02-18 15:28:43 -0500

    fixing migration error --silly capitalization

--- db/migrate/20140217030058_create_comments.rb
+++ db/migrate/20140217030058_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.belongs_to :User, index: true
-      t.belongs_to :Post, index: true
+      t.belongs_to :user, index: true
+      t.belongs_to :post, index: true
 
       t.timestamps
     end
